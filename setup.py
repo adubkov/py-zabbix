51cf9ffde279823a516137ce74dbfc0d2eb1948d
Alexey Dubkov <alexey.dubkov@elastica.net>
2015-02-11 21:52:24 -0800

    Add README.md

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name='zabbix',
+setup(name='py-zabbix',
       version='0.5.1',
       description='Python modules for work with zabbix.',
-      url='https://github.com/blacked/zabbix',
+      url='https://github.com/blacked/py-zabbix',
       author='Alexey Dubkov',
       author_email='alexey.dubkov@gmail.com',
       packages=['zabbix']
