cce549fde985f26efa472c5c5774fda98f3b29ea
Maksim Syomochkin <maksim77ster@gmail.com>
2016-04-20 10:28:09 +0300

    Add API test over SSL

--- tests/test_Functional_API.py
+++ tests/test_Functional_API.py
@@ -14,6 +14,14 @@ class FunctionalAPI(TestCase):
         except ZabbixAPIException:
             self.fail('Can\'t login to Zabbix')
 
+    def test_LoginToServerSSL(self):
+        try:
+            ZabbixAPI(url='http://127.0.0.1',
+                      user='Admin',
+                      password='zabbix')
+        except ZabbixAPIException:
+            self.fail('Can\'t login to Zabbix')
+
     def test_get_api_version(self):
         zapi = ZabbixAPI(url='http://127.0.0.1',
                          user='Admin',
