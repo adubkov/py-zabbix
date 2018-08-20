865b3bb7ed59f5383fd2930f5de22b9d3b7230de
nixargh <nixargh@protonmail.com>
2018-08-20 13:04:01 +0300

    Adjust fork to my needs

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""Setup of py-zabbix."""
+
 from setuptools import setup
 from pyzabbix import __version__
 
-setup(name='py-zabbix',
-      version=__version__,
-      description='Python module to work with zabbix.',
-      long_description=open('README.rst', 'r').read() + '\n\n' + open(
-              'CHANGELOG.rst', 'r').read(),
-      url='https://github.com/blacked/py-zabbix',
-      author='Alexey Dubkov',
-      author_email='alexey.dubkov@gmail.com',
-      test_suite='tests',
-      packages=['pyzabbix', 'zabbix'],
-      tests_require=['mock'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Topic :: System :: Monitoring',
-          'Topic :: System :: Networking :: Monitoring',
-          'Topic :: System :: Systems Administration'
-      ]
-      )
+setup(
+    name='py-zabbix',
+    version=__version__,
+    description='Python module to work with zabbix.',
+    long_description="It's a fork of https://github.com/adubkov/py-zabbix" +
+    "\n\n" +
+    open('README.rst', 'r').read() +
+    '\n\n' +
+    open('CHANGELOG.md', 'r').read(),
+    url='https://github.com/nixargh/py-zabbix',
+    author='nixargh',
+    author_email='nixargh@protonmail.com',
+    test_suite='tests',
+    packages=['pyzabbix'],
+    tests_require=['mock'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Networking :: Monitoring',
+        'Topic :: System :: Systems Administration'
+    ])
--- zabbix/api.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-from pyzabbix import ZabbixAPI
-
-warnings.warn("Module '{name}' was deprecated, use 'pyzabbix' instead."
-              "".format(name=__name__), DeprecationWarning)
